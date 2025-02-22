bd8a9586d8bd57ccebf981f80abbd98e06e6fb9d
= <=>
2025-02-21 22:27:18 -0300

    feat: Criada a página de arquivo

---
+++ NotesApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace NotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CounterBtn_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
